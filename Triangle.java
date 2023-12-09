7261569c56c1cd63eeb460d9dbf8ff3273669add
shabrinaq <shabrinaqottrunnada@gmail.com>
2023-12-09 17:55:28 +0700

    new modify

--- Triangle.java
+++ Triangle.java
@@ -6,7 +6,7 @@ public class Triangle {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.print("Enter the value of N: ");
         int N = sc.nextInt();
         int i = 0;
@@ -19,5 +19,6 @@ public class Triangle {
             System.out.println();
             i++; 
         }
+        close();
     }
-}
+} 
